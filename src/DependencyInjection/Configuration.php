6bc510b3d19066b8176018ccb2ff69e7e1865fc8
Joran Beaufort <mbaer@geonet.ch>
2016-07-24 14:25:50 +0200

    files

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('neo4j_user');
+        $rootNode = $treeBuilder->root('neo4j_php_ogm_test');
         
         $rootNode
             ->children()
