5c8b6365ec07e6ad5b8c90a4d441f88d4ab35ddf
Joran Beaufort <mbaer@geonet.ch>
2016-07-24 13:52:21 +0200

    file

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JoranBeaufort\Neo4jUserBundle\DependencyInjection;
+namespace JoranBeaufort\Neo4jPhpOgmTestBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
